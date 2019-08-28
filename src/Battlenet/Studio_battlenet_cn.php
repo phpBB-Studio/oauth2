d80135720e9d1536f3086fa0140851d0c23b4b8d
3D-I <480857+3D-I@users.noreply.github.com>
2019-08-29 00:58:33 +0200

    Add Battlenet

---
+++ src/Battlenet/Studio_battlenet_cn.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *
+ * phpBB Studio - Battle.net OAuth2 light. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2019, phpBB Studio, https://www.phpbbstudio.com
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace OAuth\OAuth2\Service;
+
+use OAuth\Common\Consumer\Credentials;
+use OAuth\Common\Http\Client\ClientInterface;
+use OAuth\Common\Http\Uri\Uri;
+use OAuth\Common\Http\Uri\UriInterface;
+use OAuth\Common\Storage\TokenStorageInterface;
+
+class Studio_battlenet_cn extends Studio_battlenet_base
+{
+	public function __construct(
+		Credentials $credentials,
+		ClientInterface $http_client,
+		TokenStorageInterface $storage,
+		$scopes = [],
+		UriInterface $base_api_uri = null
+	)
+	{
+		parent::__construct($credentials, $http_client, $storage, $scopes, $base_api_uri);
+
+		$this->region = 'cn';
+		$this->baseApiUri = new Uri('https://www.battlenet.com.cn/');
+	}
+
+	/**
+	 * @return \OAuth\Common\Http\Uri\UriInterface
+	 */
+	public function getAuthorizationEndpoint()
+	{
+		return new Uri('https://www.battlenet.com.cn/oauth/authorize');
+	}
+
+	/**
+	 * @return \OAuth\Common\Http\Uri\UriInterface
+	 */
+	public function getAccessTokenEndpoint()
+	{
+		return new Uri('https://www.battlenet.com.cn/oauth/token');
+	}
+
+}
---
+++ src/Battlenet/Studio_battlenet_us.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * phpBB Studio - Battle.net OAuth2 light. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2019, phpBB Studio, https://www.phpbbstudio.com
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace OAuth\OAuth2\Service;
+
+use OAuth\Common\Consumer\Credentials;
+use OAuth\Common\Http\Client\ClientInterface;
+use OAuth\Common\Http\Uri\Uri;
+use OAuth\Common\Http\Uri\UriInterface;
+use OAuth\Common\Storage\TokenStorageInterface;
+
+class Studio_battlenet_us extends Studio_battlenet_base
+{
+	public function __construct(
+		Credentials $credentials,
+		ClientInterface $http_client,
+		TokenStorageInterface $storage,
+		$scopes = [],
+		UriInterface $base_api_uri = null
+	)
+	{
+		parent::__construct($credentials, $http_client, $storage, $scopes, $base_api_uri);
+
+		$this->region = 'us';
+		$this->baseApiUri = new Uri('https://' . $this->region . '.battle.net/');
+	}
+}
